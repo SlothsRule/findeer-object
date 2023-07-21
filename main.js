c62439989c6e285a4701c9bc8a03e3435f96b6f1
SlothsRule <98575542+SlothsRule@users.noreply.github.com>
2023-07-21 16:41:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+video="";
+Status=false;
+
+function preload(){
+video=createVideo("video.mp4");
+video.hide();
+}
+
+
+
+
+
+function setup(){
+   canvas=createCanvas(480,380);
+   canvas.center(); 
+}
+
+
+
+
+
+
+function draw(){
+   image(video,0,0,480,380) 
+}
+
+function start(){
+  objectDetector= ml5.objectDetector('cocossd',modelLoaded);
+  document.getElementById("status").innerHTML="Status:detecting objects";
+
+    }
+
+    function modelLoaded(){
+        console.log("model is loaded")
+        Status=true;
+        video.loop();
+        video.volume(0);
+        video.speed(1);
+    }
